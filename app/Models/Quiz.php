116675a71e125c08c8eb056e94d25f13302aa543
leonardo <dev.leonardomoraes@gmail.com>
2025-11-29 00:24:48 -0300

    models and migrations

---
+++ app/Models/Quiz.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    protected $fillable = ['title', 'description', 'slug'];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function results()
+    {
+        return $this->hasMany(Result::class);
+    }
+}
